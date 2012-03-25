3764edaef761ae7a39f47c6e7eb932700d5e3c6d
Jonathan Lozinski <jonathan.lozinski@sage.com>
2012-03-25 12:56:52 +0100

    Added test for restoring output

--- bootstrap/capture_test.rb
+++ bootstrap/capture_test.rb
@@ -7,4 +7,16 @@ result = capture do
   print 'expected'
 end
 
+expect result == 'expected'
 print check result == 'expected'
+
+#Test that output is restored afterwards
+after = capture do
+  inside = capture do
+    print 'inside'
+  end
+  print 'after'
+end
+
+expect after == 'after'
+print check after == 'after'
