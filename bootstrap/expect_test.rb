fd41a9c134b5f2b7fd0d7c80b76bb105a9d71000
Jonathan Lozinski <jonathan.lozinski@sage.com>
2012-03-29 10:50:49 +0100

    WIP on passing conditions into expect

--- bootstrap/expect_test.rb
+++ bootstrap/expect_test.rb
@@ -20,3 +20,13 @@ rescue
 end
 
 print check error_raised
+
+#Check that expect accepts a condition, not just boolean
+error_raised = false
+begin
+  expect 'something'.to_be 'something'
+rescue
+  error_raised = true
+end
+
+print check !error_raised
